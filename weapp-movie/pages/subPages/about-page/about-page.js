8224620683d4fa20e5e26aec7c580f6812fe7716
SK_ERIC <974264052@qq.com>
2019-09-08 19:25:15 +0800

    精仿

---
+++ weapp-movie/pages/subPages/about-page/about-page.js
@@ -0,0 +1,22 @@
+Page({
+  data:{
+    info:[
+      {
+        label:'姓名',
+        value:'史康'
+      },
+      {
+        label: '职业',
+        value: 'web前端'
+      },
+      {
+        label: 'QQ',
+        value: '974264052'
+      },
+      {
+        label: '微信',
+        value: '18751556890'
+      }
+    ]
+  }
+})
---
+++ weapp-movie/pages/tabBar/user/user.js
@@ -0,0 +1,8 @@
+Page({
+  onShareAppMessage(res){
+    return {
+      title:'电影',
+      path:'pages/tabBar/movie/movie'
+    }
+  }
+})
